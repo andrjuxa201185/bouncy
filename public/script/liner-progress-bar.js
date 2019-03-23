5791527fcc3ef493dda68f05fc6a51db2843e0ad
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-03-23 15:41:38 +0200

    add liner progress bar js file

--- public/script/liner-progress-bar.js
+++ public/script/liner-progress-bar.js
@@ -1,34 +1,36 @@
-class AnimateRound {
+class AnimateLine{
     constructor(canvas, elem, max) {
         this.elem = elem;
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.raf = null;
-        this.startAngle = 1.5;
-        this.max = (2 * max / 100) + 1.5; 
-        this.speed = max / 7000;
+        this.startAngle = 0;
+        this.max = max;
+        this.speed = max / 120;
     }
 
-    drow(angle) {
+    drow(max) {
         this.ctx.beginPath();
+        this.ctx.strokeStyle = '#b2b3b381';
         this.ctx.lineWidth = 4;
-        this.ctx.strokeStyle = '#d1d8e4';
-        this.ctx.arc(75, 75, 50, 0, 2 * Math.PI);
+        this.ctx.moveTo(0, 3);
+        this.ctx.lineTo(this.canvas.width, 3);
         this.ctx.stroke();
         this.ctx.beginPath();
-        this.ctx.lineWidth = 5;
         this.ctx.strokeStyle = '#19bd9a';
-        this.ctx.arc(75, 75, 50, 1.5 * Math.PI, angle * Math.PI);
+        this.ctx.lineWidth = 5;
+        this.ctx.moveTo(0, 3);
+        this.ctx.lineTo(this.canvas.width * max / 100, 3);
         this.ctx.stroke();
     }
 
     animate() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
         this.drow(this.startAngle);
-        this.elem.innerHTML = Math.round((this.startAngle - 1.5) / 2 * 100) + '%';
+        this.elem.innerHTML = Math.round(this.startAngle) + '%';
         this.startAngle += this.speed;
         if (this.startAngle > this.max + this.speed) {
-            cancelAnimationFrame(this.raf);
+            window.cancelAnimationFrame(this.raf);
             this.raf = null;
             return;
         };
@@ -36,21 +38,21 @@ class AnimateRound {
     }
 }
 
-function startProgressBar() {
-    const canvas = document.querySelectorAll('.active .round-progress');
-    const showPercent = document.querySelectorAll('.active .show-percent');
+function startLinerProgressBar() {
+    const canvas = document.querySelectorAll('.active .liner-progress');
+    const showPercent = document.querySelectorAll('.active .percent');
 
     for (let i = 0; i < canvas.length; i++) {
-        canvas[i].width = 150;
-        canvas[i].height = 150;
-        new AnimateRound(canvas[i], showPercent[i], 60 + Math.random() * 40).animate();
+        canvas[i].width = 350;
+        canvas[i].height = 6;
+        new AnimateLine(canvas[i], showPercent[i], 40 + Math.random() * 60).animate();
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const windowHeight = window.innerHeight;
-    const tabNavServices = document.querySelector('.services .tab-nav');
-    const blockRoundProgress = document.querySelector('.services .tab-content');
+    const tabNavServices = document.querySelector('.team .tab-nav');
+    const blockRoundProgress = document.querySelector('.team .tab-content');
     let flag = false;
 
     document.addEventListener('scroll', () => {
@@ -58,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if ( blockCoord < windowHeight && blockCoord > -100) {
             if (!flag) {
-                startProgressBar();
+                startLinerProgressBar();
                 flag = true;
             }
         } else {
@@ -67,8 +69,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     tabNavServices.addEventListener('click', () => {
-        startProgressBar();
-    });
-
-    
+        startLinerProgressBar();
+    });    
 });
