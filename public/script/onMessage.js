63da186c2aa18dbaed07846fa7ea627ad254fb0c
Andrey Podgorniy <andrey.podgorniy1985@gmail.com>
2019-09-08 16:11:26 +0300

    resr

--- public/script/onMessage.js
+++ public/script/onMessage.js
@@ -1,14 +1,11 @@
 window.addEventListener('load', () => {
   const frameTest = document.getElementById('frameTest');
-  console.log(frameTest);
 
   window.addEventListener("message", receiveMessage, false);
 
   function receiveMessage(e) {
     console.log(e.data);
-    console.log(e);
-
-    window.daeeeee = '-----------';
+    alert(e.data.password);
 
     frameTest.onload = () => {
       console.log('send resp');
