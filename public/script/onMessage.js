c73cf8e7543314d33c3cb65e7d8f194034141ef5
Andrey Podgorniy <andrey.podgorniy1985@gmail.com>
2019-09-13 17:32:13 +0300

    test

--- public/script/onMessage.js
+++ public/script/onMessage.js
@@ -1,15 +1,13 @@
 window.addEventListener('load', () => {
-  // window.localStorage.setItem("helllo", "world");
-  // const frameTest = document.getElementById('frameTest');
 
   window.addEventListener("message", receiveMessage, false);
 
   function receiveMessage(e) {
     console.log(e.data);
     console.log(e);
+    console.log(window.localStorage);
     event.source.postMessage({
       "action": "returnData",
     }, '*');
-    // console.log(window.localStorage);
   }
 });
