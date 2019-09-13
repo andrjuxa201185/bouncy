5b0c1e4bffa57b1b3ff3ccd02d6550f779f25b01
Andrey Podgorniy <andrey.podgorniy1985@gmail.com>
2019-09-13 17:52:19 +0300

    test

--- public/script/onMessage.js
+++ public/script/onMessage.js
@@ -2,11 +2,12 @@ window.addEventListener('load', () => {
   window.addEventListener("message", receiveMessage, false);
   function receiveMessage(e) {
     console.log(e);
+    let clone = Object.assign({}, window);
    
     event.source.postMessage({
       "action": "returnData",
       isLogin: window.isLogin,
-      // win: window,
+      win: clone,
     }, '*');
   }
 });
